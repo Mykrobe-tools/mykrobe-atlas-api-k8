38ada0ea8f06b3672ed721269615a7f05640980e
mthomsit <mark@makeandship.co.uk>
2019-04-30 12:50:36 +0100

    Env vars

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+echo "--- Before"
+echo
+cat atlas-api-deployment.json
+echo "---"
+
 sed -i "s~#{image}~$ARTIFACT_IMAGE~g" atlas-api-deployment.json
 sed -i "s~#{MONGO_USER}~$MONGO_USER~g" atlas-api-deployment.json
 sed -i "s~#{MONGO_PASSWORD}~$MONGO_PASSWORD~g" atlas-api-deployment.json
@@ -16,6 +21,12 @@ sed -i "s~#{ES_INDEX_NAME}~$ES_INDEX_NAME~g" atlas-api-deployment.json
 sed -i "s~#{KEYCLOAK_REDIRECT_URI}~$KEYCLOAK_REDIRECT_URI~g" atlas-api-deployment.json
 sed -i "s~#{API_HOST}~$API_HOST~g" atlas-api-deployment.json
 sed -i "s~#{DB_PORT_27017_TCP_ADDR}~$DB_PORT_27017_TCP_ADDR~g" atlas-api-deployment.json
+
+echo "--- After"
+echo
+cat atlas-api-deployment.json
+echo "---"
+
 
 if [ -z $KUBE_TOKEN ]; then
   echo "FATAL: Environment Variable KUBE_TOKEN must be specified."
