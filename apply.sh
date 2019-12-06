3514222340f0890f67174e68157a8fc0b75800de
davidyuyuan <davidyuan@ebi.ac.uk>
2019-12-06 10:45:50 +0000

    Initial check-in.

--- apply.sh
+++ apply.sh
@@ -5,16 +5,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 kubectl create secret docker-registry \
 dockerhub \
---namespace default \
---docker-server=https://index.docker.io/v1/ \
---docker-username=msreadonly \
---docker-password=nnzGTbYRry9Fwpra \
---docker-email=readonly@makeandship.com
+  --namespace default \
+  --docker-server=https://index.docker.io/v1/ \
+  --docker-username=msreadonly \
+  --docker-password=nnzGTbYRry9Fwpra \
+  --docker-email=readonly@makeandship.com
 
-#kubectl apply -k ${DIR}/mykrobe-atlas-api-k8/static-embassy
+kubectl apply -k ${DIR}/mykrobe-atlas-api-k8/static-embassy
 #kubectl apply -k ${DIR}/mykrobe-atlas-k8/static-embassy
 #kubectl apply -k ${DIR}/mykrobe-atlas-analysis-api/k8
 
+# Debug code
 function applyall () {
   local _mfs=( "$@" )
 
@@ -22,7 +23,9 @@ function applyall () {
     kubectl apply -f ${file}
   done
 }
+#applyall ${DIR}/mykrobe-atlas-api-k8/static-embassy/*.json
 
-applyall ${DIR}/mykrobe-atlas-api-k8/static-embassy/*.json
+kubectl rollout status deployment.apps/atlas-api-deployment --timeout=60m
+
 kubectl get pv
 kubectl get all
