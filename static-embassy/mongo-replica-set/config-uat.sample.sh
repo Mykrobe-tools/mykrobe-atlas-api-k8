383e522238dee48dec4ef0ff18c59089ef797b01
mthomsit <mark@makeandship.co.uk>
2020-06-03 15:17:11 +0100

    Add sample structures

--- static-embassy/mongo-replica-set/config-uat.sample.sh
+++ static-embassy/mongo-replica-set/config-uat.sample.sh
@@ -11,4 +11,11 @@ export APP_USER=`echo -n "atlas" | base64`
 export APP_PASSWORD=`echo -n "<password>" | base64`
 export MONGO_KEY="<MONGO_KEY>"
 
+export REQUEST_CPU="1000m"
+export REQUEST_MEMORY="1Gi"
+export REQUEST_STORAGE="2Gi"
+export LIMIT_CPU="2000m"
+export LIMIT_MEMORY="2Gi"
+export LIMIT_STORAGE="4Gi"
+
 sh ./deploy-mongo.sh
