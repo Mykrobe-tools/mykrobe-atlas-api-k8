5732880ec93e1dea3af040991d9022f870c03938
Yassire Elhani <yassire@makeandship.com>
2020-06-04 14:59:02 +0100

    deploy the client

---
+++ static-embassy/atlas-client/config-dev.sample.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export NAMESPACE="mykrobe-dev"
+export PREFIX="atlas"
+export CLIENT_IMAGE="makeandship/atlas-client:15"
+export HOST="dev.mykro.be"
+
+# Pod (Deployment) resource limits
+export REQUEST_CPU="1000m"
+export REQUEST_MEMORY="4Gi"
+export REQUEST_STORAGE="2Gi"
+export LIMIT_CPU="1000m"
+export LIMIT_MEMORY="4Gi"
+export LIMIT_STORAGE="4Gi"
+
+sh ./deploy-client.sh
