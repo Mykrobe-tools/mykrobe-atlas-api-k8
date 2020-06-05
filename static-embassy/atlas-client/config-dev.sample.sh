6e606490a53dee748255b1b213f162dda3921dbd
mthomsit <mark@makeandship.co.uk>
2020-06-05 17:11:13 +0100

    Change sample

--- static-embassy/atlas-client/config-dev.sample.sh
+++ static-embassy/atlas-client/config-dev.sample.sh
@@ -2,15 +2,15 @@
 
 export NAMESPACE="mykrobe-dev"
 export PREFIX="atlas"
-export CLIENT_IMAGE="makeandship/atlas-client:14"
+export CLIENT_IMAGE="makeandship/atlas-client:18"
 export HOST="dev.mykro.be"
 
 # Pod (Deployment) resource limits
 export REQUEST_CPU="1000m"
-export REQUEST_MEMORY="4Gi"
+export REQUEST_MEMORY="1Gi"
 export REQUEST_STORAGE="2Gi"
 export LIMIT_CPU="1000m"
-export LIMIT_MEMORY="4Gi"
+export LIMIT_MEMORY="1Gi"
 export LIMIT_STORAGE="4Gi"
 
 sh ./deploy-client.sh
