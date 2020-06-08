7ce0f0a7088e92fbc4ebf4dc433ed24065cceada
Yassire Elhani <yassire@makeandship.com>
2020-06-08 12:37:59 +0100

    create the confimap from files

--- static-embassy/analysis-api/analysis/deploy-analysis.sh
+++ static-embassy/analysis-api/analysis/deploy-analysis.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# create the confimap from files
+kubectl create configmap analysis-file --from-file=NC_000962.3.fasta --from-file=NC_000962.3.gb --from-file=tb_newick.txt -n $NAMESPACE
+
 cat <<EOF | kubectl apply -f -
 ---
 apiVersion: v1
